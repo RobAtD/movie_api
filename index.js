a6f71c703e50268d6f9c3bf137427d7cfe401591
RobAtD <r.ditler@devduck.de>
2024-08-24 12:31:23 +0200

    adds new endpoints

--- index.js
+++ index.js
@@ -58,6 +58,38 @@ app.get('/movies', (req, res) => {
     res.json(movies);
 });
 
+app.get('/movies/:name', (req, res) => {
+    res.send('Successful GET request returning data on a single movie.');
+});
+
+app.get('/genres/:name', (req, res) => {
+    res.send('Successful GET request returning data on a genre.');
+});
+
+app.get('/directors/:name', (req, res) => {
+    res.send('Successful GET request returning data on a director.');
+});
+
+app.post('/users/register', (req, res) => {
+    res.send('Successful POST request to register a new user.');
+});
+
+app.put('/users/:username', (req, res) => {
+    res.send('Successful PUT request to update a users username.');
+});
+
+app.post('/users/:username/favorites', (req, res) => {
+    res.send('Successful POST request to add a movie to a users list of favorites.');
+});
+
+app.delete('/users/:username/favorites/:movieTitle', (req, res) => {
+    res.send('Successful DELETE request to remove a movie of a users list of favorites.');
+});
+
+app.delete('/users/:username', (req, res) => {
+    res.send('Successful DELETE request to deregister an existing user.');
+});
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
